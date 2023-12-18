20135bd13e70b1ad14b93270a3605e88cd045527
petros1921 <petrosworku19@gmail.com>
2023-12-18 02:26:52 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result

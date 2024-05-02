682cb913c17b3255e01e3e24bcc455a08a3dba37
Petros Worku <139710819+petros1921@users.noreply.github.com>
2024-05-02 22:52:49 +0300

    Updated 0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,6 @@
 // Updates the text color of the <header> element to red (#FF0000)
 
-document.querySelector('head').style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});

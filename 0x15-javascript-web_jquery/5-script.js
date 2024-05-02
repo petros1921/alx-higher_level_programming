a6a2ae0a8a217f0c8012ced0c19cb1ffd918d391
petros1921 <petrosworku19@gmail.com>
2024-05-02 06:24:37 -0700

    all

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+//script that adds a <li> element to a list when the user clicks on the tag DIV#add_item
+
+$('div#add_item').click(function () {
+    let element = '<li>Item</li>';
+    $('ul.my_list').append(element);
+  });
